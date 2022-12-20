a7d16ceddb660746ad55f9b0af9bd2701ba34802
NormanZ <1275133384@qq.com>
2022-12-20 22:06:42 +0800

    update demo

--- Group2/demo.py
+++ Group2/demo.py
@@ -6,7 +6,7 @@ import dlib
 import imutils
 import argparse
 from imutils import face_utils
-from scipy.spatial import distance as dist
+from gaze_tracking.mouth import mouth_aspect_ratio
 
 
 def parse_args():
@@ -19,22 +19,6 @@ def parse_args():
 
     return parser.parse_args()
 
-
-def mouth_aspect_ratio(mouth):
-    # compute the euclidean distances between the two sets of
-    # vertical mouth landmarks (x, y)-coordinates
-    A = dist.euclidean(mouth[2], mouth[10])  # 51, 59
-    B = dist.euclidean(mouth[4], mouth[8])  # 53, 57
-
-    # compute the euclidean distance between the horizontal
-    # mouth landmark (x, y)-coordinates
-    C = dist.euclidean(mouth[0], mouth[6])  # 49, 55
-
-    # compute the mouth aspect ratio
-    mar = (A + B) / (2.0 * C)
-
-    # return the mouth aspect ratio
-    return mar
 
 args = parse_args()
 gaze = GazeTracking()
